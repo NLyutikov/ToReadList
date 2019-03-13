737846be8fc40dc89d90faf0b6c4d51fa3a968cb
Vlad Kortikov <vo@appkode.ru>
2019-03-13 17:06:16 +0200

    [routing] Move routing from ui-core to ui

--- ui/src/main/kotlin/routing/AppRoute.kt
+++ ui/src/main/kotlin/routing/AppRoute.kt
@@ -1,4 +1,4 @@
-package ru.appkode.base.ui.core.routing
+package ru.appkode.base.ui.routing
 
 import ru.appkode.base.ui.core.core.routing.Route
 
--- ui/src/main/kotlin/routing/ScreenKeyFactory.kt
+++ ui/src/main/kotlin/routing/ScreenKeyFactory.kt
@@ -1,12 +1,13 @@
-package ru.appkode.base.ui.core.routing
+package ru.appkode.base.ui.routing
 
 import ru.appkode.base.ui.core.core.model.ScreenKey
+import ru.appkode.base.ui.task.list.TaskListKey
 
 //TODO Убрать nullable у ScreenKey
 class ScreenKeyFactory : Function1<AppRoute, ScreenKey?> {
     override fun invoke(route: AppRoute): ScreenKey? {
         return when (route) {
-            is AppRoute.List -> null
+            is AppRoute.List -> TaskListKey()
             is AppRoute.Info -> null
         }
     }
