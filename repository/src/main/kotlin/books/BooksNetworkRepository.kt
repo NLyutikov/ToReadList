2ac9916ad18f808e53686e7cf21d98ac0ab0e729
Nikita Lyutikov <nik.kaliningrad39@gmail.com>
2019-04-11 17:17:41 +0200

    Added getBooksSearch to BooksNetworkRepository

--- repository/src/main/kotlin/books/BooksNetworkRepository.kt
+++ repository/src/main/kotlin/books/BooksNetworkRepository.kt
@@ -2,7 +2,9 @@ package ru.appkode.base.repository.books
 
 import io.reactivex.Observable
 import ru.appkode.base.entities.core.books.details.BookDetailsUM
+import ru.appkode.base.entities.core.books.search.BookSearchUM
 
 interface BooksNetworkRepository {
     fun getBookDetails(bookId: Long): Observable<BookDetailsUM>
+    fun getBookSearch(apiKey: String, text: String): Observable<BookSearchUM>
 }
--- repository/src/main/kotlin/books/BooksNetworkRepositoryImpl.kt
+++ repository/src/main/kotlin/books/BooksNetworkRepositoryImpl.kt
@@ -3,7 +3,9 @@ package ru.appkode.base.repository.books
 import io.reactivex.Observable
 import ru.appkode.base.data.network.books.BooksApi
 import ru.appkode.base.entities.core.books.details.BookDetailsUM
-import ru.appkode.base.entities.core.books.toUiModel
+import ru.appkode.base.entities.core.books.details.toUiModel
+import ru.appkode.base.entities.core.books.search.BookSearchUM
+import ru.appkode.base.entities.core.books.search.toUiModel
 import ru.appkode.base.ui.core.core.util.AppSchedulers
 
 class BooksNetworkRepositoryImpl(
@@ -14,6 +16,11 @@ class BooksNetworkRepositoryImpl(
     override fun getBookDetails(bookId: Long): Observable<BookDetailsUM> {
         return booksApi.getBooksDetails(bookId)
             .map { book -> book.toUiModel() }
+    }
+
+    override fun getBookSearch(apiKey: String, text: String): Observable<BookSearchUM> {
+        return booksApi.getBooksSearch(apiKey, text)
+            .map { list -> list.toUiModel() }
     }
 
 }
