d03141af83ae952093dc863ee32de9160873cf3e
Nikita Lyutikov <nik.kaliningrad39@gmail.com>
2019-04-22 16:33:43 +0200

    Add logic for swipe background

--- ui/src/main/kotlin/books/lists/adapters/Swipe.kt
+++ ui/src/main/kotlin/books/lists/adapters/Swipe.kt
@@ -1,11 +1,13 @@
 package books.lists.adapters
 
 import android.annotation.SuppressLint
+import androidx.core.view.isVisible
 import com.h6ah4i.android.widget.advrecyclerview.swipeable.SwipeableItemAdapter
 import com.h6ah4i.android.widget.advrecyclerview.swipeable.SwipeableItemConstants
 import com.h6ah4i.android.widget.advrecyclerview.swipeable.action.SwipeResultAction
 import com.h6ah4i.android.widget.advrecyclerview.swipeable.action.SwipeResultActionDoNothing
 import com.h6ah4i.android.widget.advrecyclerview.swipeable.action.SwipeResultActionRemoveItem
+import ru.appkode.base.ui.R
 import ru.appkode.base.ui.books.lists.adapters.CommonListAdapter
 
 interface Swipe : SwipeableItemAdapter<CommonListAdapter.ViewHolder> {
@@ -40,6 +42,24 @@ interface Swipe : SwipeableItemAdapter<CommonListAdapter.ViewHolder> {
 
     @SuppressLint("SwitchIntDef")
     override fun onSetSwipeBackground(holder: CommonListAdapter.ViewHolder, position: Int, type: Int) {
+        var bgRes = 0
+        if (holder.wishListIcon.isVisible) {
+            when (type) {
+                SwipeableItemConstants.DRAWABLE_SWIPE_LEFT_BACKGROUND ->
+                    bgRes = R.drawable.wish_list_bg_swipe_item_left
+                SwipeableItemConstants.DRAWABLE_SWIPE_RIGHT_BACKGROUND ->
+                    bgRes = R.drawable.wish_list_bg_swipe_item_right
+            }
+        }
+        if (holder.historyIcon.isVisible) {
+            when (type) {
+                SwipeableItemConstants.DRAWABLE_SWIPE_LEFT_BACKGROUND ->
+                    bgRes = R.drawable.history_bg_swipe_item_left
+                SwipeableItemConstants.DRAWABLE_SWIPE_RIGHT_BACKGROUND ->
+                    bgRes = R.drawable.history_bg_swipe_item_right
+            }
+        }
+        holder.itemView.setBackgroundResource(bgRes)
     }
 }
 
